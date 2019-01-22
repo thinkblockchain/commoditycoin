a8be1a51284e5a186c9e150e4201623ab37b4894
thinkblockchain <46258503+thinkblockchain@users.noreply.github.com>
2019-01-22 06:46:07 +0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "199.247.15.130:17236",
-  "199.247.15.130:18236",
+  "66.42.93.172",
+  "108.61.162.54",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
